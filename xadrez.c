8b608922e9bd632ca014b6851fd1abc72d2ce4e3
Teckiro <rafaelmarcilio027@gmail.com>
2025-04-09 20:56:38 +0000

    criei o prinf

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    #include <stdio.h>
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    int main(){
+        
+        int torre = 1;
+        int rainha = 1;    //variavel 
+    
+        printf("*** Jogo de Xadrez ***\n");
+        
+        printf("Mover a torre 5 casas a direita\n");   //imprime a direção do movimento da torre
+        
+        do{
+            printf("%d\n", torre);     //saída
+            torre++;
+        } while (torre <= 5);      //condição
+    
+        printf("-------------------------------------\n");
+    
+        printf("Mover a rainha 8 casas para cima\n");   //imprime a direção do movimento da rainha
+    
+        while (rainha <= 8){
+            printf("%d\n", rainha);  //saída
+            rainha++;
+        }
+        
+        printf("-------------------------------------\n");
+    
+        printf("Mover o bispo 5 casas para a diagonal (cima, direita)\n");    //imprime a direção do movimento do bispo
+    
+        for (int bispo = 1; bispo <= 5; bispo++){     //condição
+            printf("%d\n", bispo); //saída
+        }
+        
+        
+        
+    }
+    
 
     return 0;
 }
